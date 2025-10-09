0aead5e398457b451245c33c7452bc99870b8071
zheruizz <liuksszzr@gmail.com>
2025-10-09 12:23:45 -0400

    fix CORS issue in /backend

--- backend/hello.ts
+++ backend/hello.ts
@@ -3,7 +3,10 @@ import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   return {
     statusCode: 200,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify({ message: "Hello from backend!" }),
   };
 };
