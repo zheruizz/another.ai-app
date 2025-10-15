33478d788dc801026610c69148a9eeb08fa42a6c
zheruizz <liuksszzr@gmail.com>
2025-10-15 16:50:48 -0400

    update backend stack for db config

--- infra/backend-stack.ts
+++ infra/backend-stack.ts
@@ -2,11 +2,28 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // VPC, Subnets, Security Group for Lambda <-> RDS
+    const vpc = ec2.Vpc.fromVpcAttributes(this, "AppVPC", {
+      vpcId: "vpc-004303f540511f4e6",
+      availabilityZones: ["us-east-1d"], // You can add more if needed
+      privateSubnetIds: [
+        "subnet-0b12559a29fa04790",
+        "subnet-043f5252ea218d1da",
+        "subnet-0ebc8ff15884a84b9",
+        "subnet-0f2c9953441f05d8c",
+        "subnet-08a1a7e857a96af6a",
+        "subnet-00aadc11d067e33ac",
+      ],
+    });
+
+    const lambdaSG = ec2.SecurityGroup.fromSecurityGroupId(this, "LambdaSG", "sg-0dedb76750f96ebbd");
 
     // S3 bucket for /upload demo
     const demoBucket = new s3.Bucket(this, "DemoBucket", {
@@ -14,12 +31,33 @@ export class BackendStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
+    // Common DB environment variables
+    const dbEnv = {
+      DB_HOST: "another-ai-db.c6bec2a8cygx.us-east-1.rds.amazonaws.com",
+      DB_PORT: "5432",
+      DB_USER: "master",
+      DB_PASSWORD: "OniiohnVY^8134#$45naf^3e", // For production, use Secrets Manager!
+      DB_NAME: "postgres", // Change if your DB name is different
+      DB_SSL: "true"
+    };
+
     // Lambda for /hello and /upload
     const helloLambda = new lambda.Function(this, "HelloLambda", {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: "dist/hello.handler",
       code: lambda.Code.fromAsset("../backend"),
+      vpc,
+      securityGroups: [lambdaSG],
+      vpcSubnets: { subnets: [
+        ec2.Subnet.fromSubnetId(this, "subnet1", "subnet-0b12559a29fa04790"),
+        ec2.Subnet.fromSubnetId(this, "subnet2", "subnet-043f5252ea218d1da"),
+        ec2.Subnet.fromSubnetId(this, "subnet3", "subnet-0ebc8ff15884a84b9"),
+        ec2.Subnet.fromSubnetId(this, "subnet4", "subnet-0f2c9953441f05d8c"),
+        ec2.Subnet.fromSubnetId(this, "subnet5", "subnet-08a1a7e857a96af6a"),
+        ec2.Subnet.fromSubnetId(this, "subnet6", "subnet-00aadc11d067e33ac"),
+      ]},
       environment: {
+        ...dbEnv,
         BUCKET_NAME: demoBucket.bucketName,
       },
     });
@@ -29,6 +67,17 @@ export class BackendStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: "dist/routes/surveys.routes.handler",
       code: lambda.Code.fromAsset("../backend"),
+      vpc,
+      securityGroups: [lambdaSG],
+      vpcSubnets: { subnets: [
+        ec2.Subnet.fromSubnetId(this, "subnet1s", "subnet-0b12559a29fa04790"),
+        ec2.Subnet.fromSubnetId(this, "subnet2s", "subnet-043f5252ea218d1da"),
+        ec2.Subnet.fromSubnetId(this, "subnet3s", "subnet-0ebc8ff15884a84b9"),
+        ec2.Subnet.fromSubnetId(this, "subnet4s", "subnet-0f2c9953441f05d8c"),
+        ec2.Subnet.fromSubnetId(this, "subnet5s", "subnet-08a1a7e857a96af6a"),
+        ec2.Subnet.fromSubnetId(this, "subnet6s", "subnet-00aadc11d067e33ac"),
+      ]},
+      environment: dbEnv,
     });
 
     // Lambda for projects endpoints
@@ -36,6 +85,17 @@ export class BackendStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: "dist/routes/projects.routes.handler",
       code: lambda.Code.fromAsset("../backend"),
+      vpc,
+      securityGroups: [lambdaSG],
+      vpcSubnets: { subnets: [
+        ec2.Subnet.fromSubnetId(this, "subnet1p", "subnet-0b12559a29fa04790"),
+        ec2.Subnet.fromSubnetId(this, "subnet2p", "subnet-043f5252ea218d1da"),
+        ec2.Subnet.fromSubnetId(this, "subnet3p", "subnet-0ebc8ff15884a84b9"),
+        ec2.Subnet.fromSubnetId(this, "subnet4p", "subnet-0f2c9953441f05d8c"),
+        ec2.Subnet.fromSubnetId(this, "subnet5p", "subnet-08a1a7e857a96af6a"),
+        ec2.Subnet.fromSubnetId(this, "subnet6p", "subnet-00aadc11d067e33ac"),
+      ]},
+      environment: dbEnv,
     });
 
     // Lambda for personas endpoints
@@ -43,6 +103,17 @@ export class BackendStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: "dist/routes/personas.routes.handler",
       code: lambda.Code.fromAsset("../backend"),
+      vpc,
+      securityGroups: [lambdaSG],
+      vpcSubnets: { subnets: [
+        ec2.Subnet.fromSubnetId(this, "subnet1n", "subnet-0b12559a29fa04790"),
+        ec2.Subnet.fromSubnetId(this, "subnet2n", "subnet-043f5252ea218d1da"),
+        ec2.Subnet.fromSubnetId(this, "subnet3n", "subnet-0ebc8ff15884a84b9"),
+        ec2.Subnet.fromSubnetId(this, "subnet4n", "subnet-0f2c9953441f05d8c"),
+        ec2.Subnet.fromSubnetId(this, "subnet5n", "subnet-08a1a7e857a96af6a"),
+        ec2.Subnet.fromSubnetId(this, "subnet6n", "subnet-00aadc11d067e33ac"),
+      ]},
+      environment: dbEnv,
     });
 
     demoBucket.grantReadWrite(helloLambda);
