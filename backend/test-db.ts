801788f010f6ca13ab8b380e95b59d1bcf947c89
zheruizz <liuksszzr@gmail.com>
2025-10-16 10:42:09 -0400

    update connectivity test to include real query

--- backend/test-db.ts
+++ backend/test-db.ts
@@ -3,7 +3,7 @@ import db from "./utils/db";
 
 export const handler: APIGatewayProxyHandlerV2 = async () => {
   try {
-    const result = await db.query("SELECT 1 as test;");
+    const result = await db.query("SELECT * FROM projects;");
     return {
       statusCode: 200,
       body: JSON.stringify({ success: true, result: result.rows }),
