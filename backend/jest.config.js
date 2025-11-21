e3276567ed33befe1e9a39e835037623e4f9e60d
MBZ-0 <mohamed.sbeinati@mail.utoronto.ca>
2025-11-21 16:15:10 -0500

    changed jest.config.js for ci to work

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,11 @@
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'], // Test file pattern
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  clearMocks: true,
+  // if you have other config, keep it; just add these:
+  roots: ["<rootDir>/src"],
+
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/lambda-package/"],
+  modulePathIgnorePatterns: ["<rootDir>/lambda-package/"],
+
+  // keep whatever else was there (transform, preset, etc.)
 };
